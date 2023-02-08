a8f8b0adbb86337ba322f6d63a5fe795c70089d2
ADITYA0920 <adityamane21880@gmail.com>
2023-02-08 20:47:58 +0530

    overloding method

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -4,5 +4,27 @@ import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
-     
+    public static void main(String[] args) {
+        Product p=new Product();
+        p.product(1,2);
+        //System.out.println(p.product(1,2));
+        p.product(1,2,3);
+        //System.out.println(p.product(1,2,3));
+        p.product(1.2,2.2);
+        System.out.println(p.product(1.2,2.2));
+
+
+    }
+static class Product{
+
+    public int product(int x, int y) {
+        return 0;
+    }
+    public int product(int x, int y, int z) {
+        return 0;
+    }
+    public double product(double x, double y) {
+        return (double) 0;
+    }
+}
 }
