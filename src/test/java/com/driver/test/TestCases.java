a9600491cf29d5fd861467f9419d0655fdfc5eb4
ADITYA0920 <adityamane21880@gmail.com>
2023-02-10 00:22:59 +0530

    polymorphism 2nd commit

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -7,9 +7,9 @@ public class TestCases {
     public static void main(String[] args) {
         Product p=new Product();
         p.product(1,2);
-        //System.out.println(p.product(1,2));
+        System.out.println(p.product(1,2));
         p.product(1,2,3);
-        //System.out.println(p.product(1,2,3));
+        System.out.println(p.product(1,2,3));
         p.product(1.2,2.2);
         System.out.println(p.product(1.2,2.2));
 
@@ -18,13 +18,13 @@ public class TestCases {
 static class Product{
 
     public int product(int x, int y) {
-        return 0;
+        return x*y;
     }
     public int product(int x, int y, int z) {
-        return 0;
+        return x*y*z;
     }
     public double product(double x, double y) {
-        return (double) 0;
+        return  (double)x*y;
     }
 }
 }
