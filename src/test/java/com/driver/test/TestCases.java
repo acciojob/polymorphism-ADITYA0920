3786936a823f8028531d776202c43822b44815ad
ADITYA0920 <adityamane21880@gmail.com>
2023-02-12 19:09:44 +0530

    second attemt

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -5,29 +5,29 @@ import org.junit.jupiter.api.Test;
 
 public class TestCases {
 
+    public static class Product{
 
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return  x*y;
+        }
+    }
     public static void main(String[] args) {
 
-         class Product{
 
-            public int product(int x, int y) {
-                return x*y;
-            }
-            public int product(int x, int y, int z) {
-                return x*y*z;
-            }
-            public double product(double x, double y) {
-                return  (double)x*y;
-            }
-        }
 
         Product p=new Product();
         p.product(1,2);
-        System.out.println(p.product(1,2));
+       // System.out.println(p.product(1,2));
         p.product(1,2,3);
-        System.out.println(p.product(1,2,3));
+       // System.out.println(p.product(1,2,3));
         p.product(1.2,2.2);
-        System.out.println(p.product(1.2,2.2));
+       // System.out.println(p.product(1.2,2.2));
 
 
     }
